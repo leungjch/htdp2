1e4036d1b1eb4574b9ead8dc4fbb69a9fc58c43a
Justin Leung <justin.leung.ch@gmail.com>
2020-08-14 19:32:48 -0400

    E36-38

---
+++ 1-fixed-size-data/e36_image_area.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname e36_image_area) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+; image -> number
+; image-area, counts number of pixels
+
+(define (image-area img)
+  (* (image-width img) (image-height img)))
---
+++ 1-fixed-size-data/e38_string_remove_last_character.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname e38_string_remove_last_character) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+; string -> string
+; produce a string with the last character removed
+(define (string-rest str)
+  (substring str 0 (- (string-length str) 2)))
+(string-rest "hello")
