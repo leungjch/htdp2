c6bf88bc9a65a9f5d8085c5da027390f3a9a1630
Justin Leung <justin.leung.ch@gmail.com>
2020-08-14 11:09:06 -0400

    fixed size exercises

---
+++ 1-fixed-size-data/e1_distance.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname e1) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+; Exercise 1. Compute distance
+(define x 3)
+(define y 4)
+
+(define (distance myX myY)
+  (sqrt (+ (expt myX 2) (expt myY 2))))
+(distance x y)
