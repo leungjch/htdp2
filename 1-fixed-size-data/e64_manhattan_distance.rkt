7063369c4f80eb142f79f20bde972158d010b643
Justin Leung <justin.leung.ch@gmail.com>
2020-08-15 00:58:02 -0400

    E64

---
+++ 1-fixed-size-data/e64_manhattan_distance.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname e64_manhattan_distance) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+(define (manhattan-distance pos)
+  (+ (posn-x pos) (posn-y pos)))
+(manhattan-distance (make-posn 3 3))
