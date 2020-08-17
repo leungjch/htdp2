509adddd59be035c32821f0705d178fff1ae2161
Justin Leung <justin.leung.ch@gmail.com>
2020-08-17 13:18:01 -0400

    multiplication

--- 2-arbitrary-size-data/e151b_multiplication_succ.rkt
+++ 2-arbitrary-size-data/e151b_multiplication_succ.rkt
@@ -1,19 +1,22 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname e151b_multiplication_succ) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
-(define (num x)
+; NaturalNumber -> Number
+; add a number A to a natural number B without using the primitive + operation
+(define (my-add a b)
   (cond
-    [(zero? x) x]
-    [else (add1 (sub1 x))]))
-(num 10)
+    [(zero? a) b]
+    [else (add1 (my-add (sub1 a) b))]))
+; Tests
+(check-expect (my-add 4 3) 7)
 
 ; NaturalNumber Number -> Number
 ; multiply a natural number n by a number x without using *
 (define (multiply n x)
   (cond
     [(zero? n) 0]
-    [else (num (multiply (sub1 n) x))]))
-(multiply 10 3)
+    [else (my-add x (multiply (sub1 n) x))]))
+(multiply 10 30)
 
-;(check-within (add-to-pi 3) (+ 3 pi) 0.001)
+(check-expect (multiply 10 30) 300)
  
